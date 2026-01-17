af5f10f04443107127432dbe69a3c1a7d005d5b4
Professor <superelitealfa@gmail.com>
2026-01-17 02:37:31 +0100

    init

---
+++ api-file/src/test/java/com/albert/api_file/ApiFileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.albert.api_file;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
