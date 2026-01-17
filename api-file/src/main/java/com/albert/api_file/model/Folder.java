4f617fdd70c5a3fe2e658ceab51da052b9b9f1d4
Professor <superelitealfa@gmail.com>
2026-01-17 03:20:13 +0100

    folder model completed

--- api-file/src/main/java/com/albert/api_file/model/Folder.java
+++ api-file/src/main/java/com/albert/api_file/model/Folder.java
@@ -1,7 +1,38 @@
 package com.albert.api_file.model;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
 public class Folder {
+
+    @Id
+    private final UUID id = UUID.randomUUID();
+
+    @Column(unique = true)
+    private String name;
+
+    @Column
+    private int content;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    private User user;
+
+    @Column(nullable = false)
+    private LocalDateTime created_At;
+
+    public Folder(String name, int content, User user, LocalDateTime created_At){
+        this.name = name;
+        this.content = content;
+        this.user = user;
+        this.created_At = created_At;
+    }
 }
