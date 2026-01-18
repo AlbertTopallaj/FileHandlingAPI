263ef1f160371629dce5f0af7c5b5a6964184a38
Professor <altopallaj21@gmail.com>
2026-01-18 22:25:48 +0100

    changed file repo to interface

--- api-file/src/main/java/com/albert/api_file/repository/FileRepository.java
+++ api-file/src/main/java/com/albert/api_file/repository/FileRepository.java
@@ -1,5 +1,13 @@
 package com.albert.api_file.repository;
 
-public class FileRepository {
+import com.albert.api_file.model.File;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface FileRepository extends JpaRepository<File, UUID> {
+    List<File> findByTitleContaining(String title);
 }
--- api-file/src/main/java/com/albert/api_file/service/FileService.java
+++ api-file/src/main/java/com/albert/api_file/service/FileService.java
@@ -1,6 +1,8 @@
 package com.albert.api_file.service;
 
+import com.albert.api_file.model.File;
 import com.albert.api_file.repository.FileRepository;
+import com.albert.api_file.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -10,5 +12,11 @@ public class FileService {
 
     private final FileRepository fileRepository;
     private final UserRepository userRepository;
+
+    public File createFile(CreateFileRequest request){
+        if (request.getTitle.isBlank()) {
+            throw new IllegalArgumentException("Title may not be blank");
+        }
+    }
 
 }
