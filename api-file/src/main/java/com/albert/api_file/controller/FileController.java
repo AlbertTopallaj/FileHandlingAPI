895dc4047797e2838951cf8b62df76dced62ef6b
Professor <altopallaj21@gmail.com>
2026-01-18 21:12:04 +0100

    FileController first endpoint under progress

--- api-file/src/main/java/com/albert/api_file/controller/FileController.java
+++ api-file/src/main/java/com/albert/api_file/controller/FileController.java
@@ -1,4 +1,38 @@
 package com.albert.api_file.controller;
 
+import com.albert.api_file.service.FileService;
+import com.albert.api_file.service.FolderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/file")
+@RequiredArgsConstructor
 public class FileController {
+
+    private final FileService fileService;
+    private final FolderService folderService;
+
+    @PostMapping
+    public ResponseEntity<?> createFile(@RequestBody CreateFileRequest request){
+        try {
+            var file = fileService.createFile(file);
+
+            return ResponseEntity.created(URI.create("/file")).body(FileResponse.from(file));
+        } catch (IllegalArgumentException exception) {
+            return ResponseEntity.badRequest()
+                    .body(Map.of("error", exception.getMessage()));
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            return Respons
+        }
+    }
+
 }
