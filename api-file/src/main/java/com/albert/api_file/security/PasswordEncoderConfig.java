06e5b816d9f126f85ea0f79fc704f28a4c958c6d
Professor <altopallaj21@gmail.com>
2026-01-27 04:21:58 +0100

    removed unnecceary file

--- api-file/src/main/java/com/albert/api_file/security/PasswordEncoderConfig.java
+++
@@ -1,8 +0,0 @@
-package com.albert.api_file.security;
-
-import org.springframework.security.crypto.bcrypt.BCrypt;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-public class PasswordEncoderConfig {
-
-}
