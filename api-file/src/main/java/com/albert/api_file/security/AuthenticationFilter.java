4013def5c0c44bcb1106f49fa7c5a0fff3e81b66
Professor <altopallaj21@gmail.com>
2026-01-27 04:43:07 +0100

    changes to AUTHENTICATIONFILTER

--- api-file/src/main/java/com/albert/api_file/security/AuthenticationFilter.java
+++ api-file/src/main/java/com/albert/api_file/security/AuthenticationFilter.java
@@ -1,6 +1,9 @@
 package com.albert.api_file.security;
 
+import com.albert.api_file.models.User;
+import com.albert.api_file.repositories.IUserRepository;
 import com.albert.api_file.services.UserService;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -11,31 +14,52 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Optional;
+import java.util.UUID;
 
 @RequiredArgsConstructor
 public class AuthenticationFilter extends OncePerRequestFilter {
 
-    private final UserService userService;
+    private final JWTService jwtService;
+    private final IUserRepository userRepository;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain)
+            throws ServletException, IOException {
+
         String authenticationHeader = request.getHeader("Authorization");
         if (authenticationHeader == null || authenticationHeader.isBlank() || authenticationHeader.length() <= "Bearer ".length()) {
             filterChain.doFilter(request, response);
             return;
         }
+
         String jwtToken = authenticationHeader.substring("Bearer ".length());
-        if (jwtToken.isBlank()) {
-            filterChain.doFilter(request, response);
+
+        UUID userId;
+        try {
+            userId = jwtService.verifyToken(jwtToken);
+        } catch (JWTVerificationException exception) {
+            response.setStatus(401);
             return;
         }
-        userService.verifyAuthentication(jwtToken).ifPresent(user ->{
-           var authentication = new UsernamePasswordAuthenticationToken(
-                   user,
-                   user.getPassword()
-           );
-            SecurityContextHolder.getContext().setAuthentication(authentication);
-        });
+
+        Optional<User> userOptional = userRepository.findById(userId);
+        if (userOptional.isEmpty()) {
+            response.setStatus(401);
+            return;
+        }
+
+        User user = userOptional.get();
+
+        SecurityContextHolder
+                .getContext()
+                .setAuthentication(new UsernamePasswordAuthenticationToken(
+                        user, user.getPassword(), new ArrayList<>()
+                ));
         filterChain.doFilter(request, response);
     }
 }
